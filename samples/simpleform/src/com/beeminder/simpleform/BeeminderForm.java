666a46e365d2272ce897520ee02b9a8769de9143
Uluc Saranli <saranli@ceng.metu.edu.tr>
2013-10-18 14:48:30 +0300

    - Revised sample and shifted relevant code to the api

--- samples/simpleform/src/com/beeminder/simpleform/BeeminderForm.java
+++ samples/simpleform/src/com/beeminder/simpleform/BeeminderForm.java
@@ -1,73 +1,112 @@
 package com.beeminder.simpleform;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.view.Menu;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.beeminder.beedroid.api.Session;
+import com.beeminder.beedroid.api.Session.SessionException;
+import com.beeminder.beedroid.api.Session.SessionState;
+
 public class BeeminderForm extends Activity {
 
 	private static final String TAG = "BeeminderForm";
-	private static final boolean LOCAL_LOGV = true;
 
-	public static final String ACTION_API_AUTHORIZE = "com.beeminder.beeminder.AUTHORIZE";
-	public static final String ACTION_API_UNAUTHORIZE = "com.beeminder.beeminder.UNAUTHORIZE";
 	public static final String ACTION_API_SUBMITPOINT = "com.beeminder.beeminder.SUBMITPOINT";
 	public static final String BEEMINDER_PACKAGE = "com.beeminder.beeminder";
 
 	public static final int ACTIVITY_BEEMINDER_AUTH = 1;
 
-	String mPackageName;
-	String mAppName;
 	TextView mGoalView;
 	TextView mTokenView;
 	EditText mValueView;
 
-	boolean mSelectionActive = false;
-
 	String mUsername;
 	String mGoalSlug;
 	String mToken;
+
+	private static final String KEY_USERNAME = "beeminder_username";
+	private static final String KEY_GOALSLUG = "beeminder_goalslug";
+
+	SharedPreferences mSP;
+	Session mSession;
+
+	private void resetFields() {
+		mToken = null;
+		mUsername = null;
+		mGoalSlug = null;
+		mGoalView.setText("Goal URI: none");
+		mTokenView.setText("Token: none");
+		mValueView.setText("0");
+	}
+
+	private class SessionStatusCallback implements Session.StatusCallback {
+		@Override
+		public void call(Session session, SessionState state) {
+			Log.v(TAG, "Beeminder status changed:" + state);
+
+			if (state == SessionState.OPENED) {
+				mToken = session.getToken();
+				mUsername = session.getUsername();
+				mGoalSlug = session.getGoalSlug();
+				Log.v(TAG, "Goal = " + mUsername + "/" + mGoalSlug + ", token="
+						+ session.getToken());
+
+				mGoalView.setText("Goal URI: " + mUsername + "/" + mGoalSlug);
+				mTokenView.setText("Token: " + mToken);
+
+				SharedPreferences.Editor edit = mSP.edit();
+				edit.putString(KEY_USERNAME, mUsername);
+				edit.putString(KEY_GOALSLUG, mGoalSlug);
+				edit.commit();
+
+			} else if (state == SessionState.CLOSED_ON_ERROR) {
+				Toast.makeText(getBaseContext(),
+						"Session closed with error: " + mSession.getError(),
+						Toast.LENGTH_SHORT).show();
+			} else if (state == SessionState.CLOSED) {
+				resetFields();
+			}
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_beeminder_form);
 
-		mReplyTo = new Messenger(new IncomingHandler());
-
 		mGoalView = (TextView) findViewById(R.id.token);
 		mTokenView = (TextView) findViewById(R.id.goaluri);
 		mValueView = (EditText) findViewById(R.id.value);
 
-		mGoalView.setText("Goal URI: none");
-		mTokenView.setText("Token: none");
-		mValueView.setText("0");
+		resetFields();
+
+		mSP = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		mUsername = mSP.getString(KEY_USERNAME, null);
+		mGoalSlug = mSP.getString(KEY_GOALSLUG, null);
 
 		try {
-			ApplicationInfo ai = getPackageManager().getApplicationInfo(
-					getPackageName(), 0);
-			mPackageName = getPackageName();
-			mAppName = getPackageManager().getApplicationLabel(ai).toString();
-		} catch (final NameNotFoundException e) {
-			mPackageName = null;
-			mAppName = null;
+			mSession = new Session(this);
+			mSession.setCallback(new SessionStatusCallback());
+
+			if (mUsername != null && mGoalSlug != null)
+				mSession.openForGoal(mUsername, mGoalSlug);
+			else resetFields();
+
+		} catch (SessionException e) {
+			resetFields();
+			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
+					.show();
 		}
+
 	}
 
 	@Override
@@ -75,145 +114,52 @@ public class BeeminderForm extends Activity {
 			Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 
-		switch (requestCode) {
-		case ACTIVITY_BEEMINDER_AUTH:
-			if (resultCode == Activity.RESULT_OK && intent != null) {
-				Bundle extras = intent.getExtras();
-
-				mToken = extras.getString("token");
-				mUsername = extras.getString("username");
-				mGoalSlug = extras.getString("slug");
-
-				mGoalView.setText("Goal URI: " + mUsername + "/" + mGoalSlug);
-				mTokenView.setText("Token: " + mToken);
-			} else {
-				mGoalView.setText("Goal URI: none");
-				mTokenView.setText("Token: none");
-			}
-			mSelectionActive = false;
-			break;
-		default:
-			break;
-		}
+		mSession.onActivityResult(requestCode, resultCode, intent);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.beeminder_form, menu);
-		return true;
-	}
-
-	public void selectGoal(View v) {
-		if (mPackageName == null || mAppName == null)
-			return;
-
-		if (mSelectionActive)
-			return;
-
-		mSelectionActive = true;
-		Intent intent = new Intent().setAction(ACTION_API_AUTHORIZE)
-				.setPackage(BEEMINDER_PACKAGE)
-				.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-				.addCategory(Intent.CATEGORY_DEFAULT)
-				.putExtra("protocol_version", "20131017")
-				.putExtra("protocol_action", "authorize_dialog")
-				.putExtra("application_name", mAppName)
-				.putExtra("package_name", mPackageName);
+	public void newSession(View v) {
 		try {
-			startActivityForResult(intent, ACTIVITY_BEEMINDER_AUTH);
-		} catch (ActivityNotFoundException e) {
-			Toast.makeText(getBaseContext(),
-					"Could not find the Beeminder app!", Toast.LENGTH_LONG)
+			resetFields();
+			mSession.close();
+			mSession.openForNewGoal();
+		} catch (SessionException e) {
+			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
 					.show();
 		}
 	}
 
-	/** Messenger for communicating with the service. */
-	private Messenger mService = null;
-	/** Flag indicating whether we have called bind on the service. */
-	private boolean mBound;
-    private Messenger mReplyTo = null;
-    
-	/** Class for interacting with the main interface of the service. */
-	private ServiceConnection mConnection = new ServiceConnection() {
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			mService = new Messenger(service);
-			mBound = true;
-		}
+	public void closeSession(View v) {
 
-		public void onServiceDisconnected(ComponentName className) {
-			mService = null;
-			mBound = false;
-		}
-	};
+		SharedPreferences.Editor edit = mSP.edit();
+		edit.remove(KEY_USERNAME);
+		edit.remove(KEY_GOALSLUG);
+		edit.commit();
+
+		resetFields();
+		mSession.close();
+	}
 
 	public void submitPoint(View v) {
-		if (!mBound)
-			return;
 
 		float value;
 
 		try {
 			value = Float.parseFloat(mValueView.getText().toString());
+			mSession.submitPoint(value, 0, null);
 		} catch (NumberFormatException e) {
 			Toast.makeText(getApplicationContext(), "Invalid value!",
 					Toast.LENGTH_SHORT).show();
-			return;
-		}
-
-		// Create and send a message to the service, using a supported 'what'
-		// value
-		Message msg = Message.obtain(null, 1, 0, 0);
-		Bundle extras = new Bundle();
-		extras.putString("pkgname", mPackageName);
-		extras.putString("token", mToken);
-		extras.putString("username", mUsername);
-		extras.putString("slug", mGoalSlug);
-		extras.putFloat("value", value);
-
-		msg.setData(extras);
-		msg.replyTo = mReplyTo;
-		
-		try {
-			mService.send(msg);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-		// Bind to the service
-
-		Intent intent = new Intent().setAction(ACTION_API_SUBMITPOINT)
-				.setPackage(BEEMINDER_PACKAGE);
-		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		if (mBound) {
-			unbindService(mConnection);
-			mBound = false;
-		}
-	}
-
-	private class IncomingHandler extends Handler {
-		@Override
-		public void handleMessage(Message msg) {
-			System.out.println("*****************************************");
-			System.out.println("Return successfully received!!!!!!");
-			System.out.println("*****************************************");
-
-			int what = msg.what;
-
-			Toast.makeText(BeeminderForm.this.getApplicationContext(),
-					"Remote Service replied (" + what + ")", Toast.LENGTH_LONG)
+		} catch (SessionException e) {
+			Toast.makeText(getApplicationContext(),
+					"Session error:" + e.getMessage(), Toast.LENGTH_SHORT)
 					.show();
 		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mSession.close();
 	}
 
 }
